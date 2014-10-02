059b534258f852d3fc4ca92deede8a15345127fd
Spotlight0xff <spotlight0xff@gmail.com>
2014-10-02 03:37:30 +0200

    install.sh can now ask the user to choose symlink or copy

--- install.sh
+++ install.sh
@@ -16,7 +16,47 @@
 # .bashrc
 
 
-if [[ $UID != 0 ]]; then
-    echo "launch $1 as root"
-    exit
-fi
+#if [[ $UID != 0 ]]; then
+#    echo "launch $1 as root"
+#    exit
+#fi
+
+
+
+
+function do_symlink()
+{
+    echo "symlinking the shit out of it!"
+}
+
+function do_copy()
+{
+    echo "copy the shit out of it!"
+}
+
+
+while true; do
+    echo "Do you want to copy the files or just symlink them?"
+    echo "[1] copy"
+    echo "[2] symlink"
+
+    echo -ne "\n[2] "
+    read method
+
+
+    case $method in
+        '1')
+            do_copy
+            ;;
+        '2')
+            do_symlink
+            ;;
+        *)
+            continue
+            ;;
+    esac
+    echo "All operations finished"
+    exit 0
+done
+
+
