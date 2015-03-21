3473606342a73e69395312f62f9181ca4b3b1507
spotlight0xff <spotlight0xff@gmail.com>
2015-03-21 23:45:29 +0100

    installscript can now install vim modules

--- install.sh
+++ install.sh
@@ -15,14 +15,26 @@ do
 done
 
 
-for t in "${programs_root[@]}"
-do
-    while true; do
-        read -p "Do you whish to install $t config as root? " yn
-        case $yn in
-            [Yy]* ) sudo stow -t / -v $t;break;;
-            [Nn]* ) break;;
-            * ) echo "Please answer yes or no";;
-        esac
-    done
-done
+#for t in "${programs_root[@]}"
+#do
+#    while true; do
+#        read -p "Do you whish to install $t config as root? " yn
+#        case $yn in
+#            [Yy]* ) sudo stow -t / -v $t;break;;
+#            [Nn]* ) break;;
+#            * ) echo "Please answer yes or no";;
+#        esac
+#    done
+#done
+
+read -p "Do you wish to install vim-YouCompleteMe?" yn
+case $yn in
+    [Yy]* ) cd vim/.vim/bundle && git clone https://github.com/Valloric/YouCompleteMe && cd YouCompleteMe && git submodule update --init --recursive && ./install.sh --omnisharp-completer --clang-completer; cd ../../../../
+esac
+
+read -p "Do you wish to install vim-MultipleCursors?" yn
+case $yn in
+    [Yy]* ) git clone https://github.com/terryma/vim-multiple-cursors vim/.vim/bundle/vim-multiple-cursors
+esac
+
+echo "Done :)"
